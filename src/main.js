2a871114274afa329653a7bd94c912526fc45aac
Rafael Shouman <rafael.borba@webcontinental.com.br>
2021-09-23 22:05:28 -0300

    fetch things

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 const a = 3;
 const b = 5;
-const c = 'asdsadsadadcfztr'
+const c = 'asdsadsadadcfztr';
 
+/* const defer = new Promise((resolve, reject) => {
+  if (true) {
+    resolve('Yeh, works here! :)');
+  } else {
+    reject(new Error('error'));
+  }
+});
+
+defer.then((data) => {
+  console.log(data);
+  return 'foo';
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err)); */
+
+const posts = fetch('https://wp.shouman.xyz/portfolios');
+posts
+  .then((data) => data.json())
+  .then((data) => {
+    /* throw new Error('Error, danger!'); */
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
+
+function fn() {
+  return (el) => el + 1;
+}
+
+fn();
 function sum() {
   return a + b + c;
 }
